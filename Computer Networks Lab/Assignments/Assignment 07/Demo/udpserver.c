b837f4c05d28cc20266cb6b9c003f650dfcec38a
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-03-09 12:20:22 +0530

    UDP Demo

---
+++ Computer Networks Lab/Assignments/Assignment 07/Demo/udpserver.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX 100
+
+void main()
+{
+	int server_fd,server_len,client_len;
+	struct sockaddr_in server_address,client_address;
+	char input[MAX],result[MAX];
+	int l=0;
+
+	bzero(&server_address,sizeof(server_address));
+
+	server_fd=socket(AF_INET,SOCK_DGRAM,0);
+	server_address.sin_family=AF_INET;
+	server_address.sin_addr.s_addr=htonl(INADDR_ANY);
+	server_address.sin_port=htons(8888);
+	server_len=sizeof(server_address);
+
+	bind(server_fd,(struct sockaddr *)&server_address,server_len);
+	printf("Server Running\n");
+
+	while(1)
+	{
+		client_len=sizeof(client_address);
+		recvfrom(server_fd,input,MAX,0,(struct sockaddr *)&client_address,&client_len);
+		printf("Server Received : %s\n",input);
+		l=strlen(input);
+		printf("%c\n",input[l-1]);
+
+		if(strcmp(input,"end")==0)
+		{
+			break;
+		}
+
+		strcpy(result,"From Server :)");
+		printf("Sending message to the client\n");
+		sendto(server_fd,result,strlen(result)+1,0,(struct sockaddr *)&client_address,client_len);
+
+	}
+
+	close(server_fd);
+	
+}
